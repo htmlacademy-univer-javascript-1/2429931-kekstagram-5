39940dcde336f0ef2284293b575e6ba19c6679b5
sofia <sobala0987654321@gmail.com>
2024-11-21 00:55:28 +0700

    Исправлены функции

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,40 @@
-console.log("firstFunction");
+// "firstFunction"
 function firstFunction(str, num){
-  console.log(str.length <= num);
+  return str.length <= num;
 }
 firstFunction('проверяемая строка', 20);
-firstFunction('проверяемая строка', 18);
-firstFunction('проверяемая строка', 10);
+// console.log(firstFunction('проверяемая строка', 20));
+// console.log(firstFunction('проверяемая строка', 18));
+// console.log(firstFunction('проверяемая строка', 10));
 
-console.log("secondFunction");
+// "secondFunction"
 function secondFunction(str){
-  var i, len;
+  let i;
 
   str = str.toLowerCase().replace(/ /g, '');
-  len = str.length;
+  const len = str.length;
 
   for(i = 0; i < len / 2; i += 1) {
-    if(str.charCodeAt(i) != str.charCodeAt(len - i - 1)) {
+    if(str.charCodeAt(i) !== str.charCodeAt(len - i - 1)) {
       return false;
     }
   }
   return true;
 }
-console.log(secondFunction('топот'));
-console.log(secondFunction('ДовОд'));
-console.log(secondFunction('Кекс'));
-console.log(secondFunction('Лёша на полке клопа нашёл '));
 
-console.log("thirdFunction");
+secondFunction('топот');
+// console.log(secondFunction('топот'));
+// console.log(secondFunction('ДовОд'));
+// console.log(secondFunction('Кекс'));
+// console.log(secondFunction('Лёша на полке клопа нашёл '));
+
+// "thirdFunction"
 function thirdFunction(numStr){
-  let str = numStr.toString();
+  const str = numStr.toString();
   let num = 0;
   let res = NaN;
   let i = 0;
-  let k = 10;
+  const k = 10;
   while(str.length - i > 0){
     if(Number.isNaN(parseInt(str[i],10)) === false){
       num = num * k + parseInt(str[i],10);
@@ -41,11 +44,12 @@ function thirdFunction(numStr){
   }
   return res;
 }
-console.log(thirdFunction("2023 год"));
-console.log(thirdFunction("ECMAScript 2022"));
-console.log(thirdFunction("1 кефир, 0.5 батона"));
-console.log(thirdFunction("агент 007"));
-console.log(thirdFunction("а я томат"));
-console.log(thirdFunction(2023));
-console.log(thirdFunction(-1));
-console.log(thirdFunction(1.5));
+thirdFunction(2023);
+// console.log(thirdFunction("2023 год"));
+// console.log(thirdFunction("ECMAScript 2022"));
+// console.log(thirdFunction("1 кефир, 0.5 батона"));
+// console.log(thirdFunction("агент 007"));
+// console.log(thirdFunction("а я томат"));
+// console.log(thirdFunction(2023));
+// console.log(thirdFunction(-1));
+// console.log(thirdFunction(1.5));
